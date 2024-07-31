a1f8365f8fd0b9d47c5daf91d9a5952d507982cc
mackjmr <mackenzie.jomard@datadoghq.com>
2024-07-31 16:44:25 +0200

    do not check tag

--- ci/scripts/ci-deploy-converged-agent.sh
+++ ci/scripts/ci-deploy-converged-agent.sh
@@ -23,7 +23,8 @@ install_agent() {
   helm_cmd="helm --debug upgrade "${releaseName}" -n "${namespace}" datadog/datadog --install \
     -f ./ci/converged-agent-values/values.yaml \
     --set datadog.tags=env:"${namespace}" \
-    --set agents.nodeSelector.\"alpha\\.eksctl\\.io/nodegroup-name\"=${nodegroup}"
+    --set agents.nodeSelector.\"alpha\\.eksctl\\.io/nodegroup-name\"=${nodegroup} \
+    --set agents.image.doNotCheckTag=true"
 
   eval $helm_cmd
 }
